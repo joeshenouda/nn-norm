1bf9efe8ddfbea1d882d18f79318660085ecf615
Joseph Shenouda <JosephShenouda@Joes-Macbook-Pro.local>
2022-07-07 10:22:24 -0500

    Changed generating labels script for dim 10

--- dataset/generate_syn_mnist.py
+++ dataset/generate_syn_mnist.py
@@ -8,9 +8,13 @@ from torchvision import datasets, transforms
 from torch.utils.data import Dataset
 import argparse
 
-torch.manual_seed(42)
-random.seed(42)
+def gen_labels(output_dim=10):
+    torch.manual_seed(42)
+    random.seed(42)
 
-mnist_labels = torch.randn(12000, 2)
-data_path = "data/"
-torch.save(mnist_labels, os.path.join(data_path,'2D_gaussian_labels.pt'))
+    mnist_labels = torch.randn(12000, output_dim)
+    data_path = "data/"
+    torch.save(mnist_labels, os.path.join(data_path,'2D_gaussian_labels_D_{}.pt'.format(output_dim)))
+
+if __name__ == "__main__":
+    gen_labels()
